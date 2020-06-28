e3ce9c111c74c2c8d3c7b33c4c31b1de26662d03
HommyDeJesus <HommyDeJesus@DESKTOP-A7S1VC8>
2020-06-28 11:43:29 -0400

    Pokemons details page added Changes in pokemon-id model Added pokemon component to retreive pokemon details by id

--- src/app/components/pokemon/pokemon-details/pokemon-details.component.ts
+++ src/app/components/pokemon/pokemon-details/pokemon-details.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Pokemon } from '../../../models/pokemon';
+import { PokemonId } from '../../../models/pokemon-id';
+import { PokemonService } from '../../../services/pokemon.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-details',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonDetailsComponent implements OnInit {
 
-  constructor() { }
+  public pokemons: Pokemon[];
+  public pokemon: PokemonId;
+  public sub: any;
 
-  ngOnInit(): void {
+  constructor(private pokemonService: PokemonService, private route: ActivatedRoute) { 
+
+  }
+
+  ngOnInit() {
+    this.getPokemonId();
+  }
+
+  getPokemonId() {
+    this.sub = this.route.params.subscribe(params => {
+      this.pokemonService
+        .getPokemonId(params.id)
+        .subscribe((data: PokemonId) => {
+          this.pokemon = data;
+        console.log(this.pokemon);
+        });
+    });
   }
 
 }
--- src/app/models/pokemon-id.ts
+++ src/app/models/pokemon-id.ts
@@ -2,8 +2,17 @@ export class PokemonId {
     name: string;
     id: number;
     sprites: Sprites;
+    stats: string;
+    types: string;
+    abilities: string;
+    height: string;
+    weight: string;
+    moves: string;
 }
 
 export class Sprites {
     front_default: string;
+    back_default: string;
+    front_shiny: string;
+    back_shiny: string;
 }
