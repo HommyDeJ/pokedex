15f87beb843f1a422b71fbcdbb0d44f40721f445
HommyDeJesus <HommyDeJesus@DESKTOP-A7S1VC8>
2020-06-28 13:41:19 -0400

    Heroku deploy part 6

--- src/server.js
+++ src/server.js
@@ -1,13 +1,33 @@
+//Install express server
 const express = require('express');
-
+const path = require('path');
+ 
 const app = express();
-
-app.use(express.static('./dist/pokedex'));
-
-app.get('/*', function (req, res) {
-    res.sendFile('index.html', { root: 'dist/pokedex' });
+ 
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/pokedex'));
+ 
+app.get('/*', function(req,res) {
+ 
+res.sendFile(path.join(__dirname+'/pokedex/index.html'));
 });
-
+ 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
-console.log(`Running on port ${process.env.PORT || 8080}`);
+
+
+
+// const express = require('express');
+
+// const app = express();
+
+// app.use(express.static('./dist/pokedex'));
+
+// app.get('/*', function (req, res) {
+//     res.sendFile('index.html', { root: 'dist/pokedex' });
+// });
+
+// app.listen(process.env.PORT || 8080);
+
+// console.log(`Running on port ${process.env.PORT || 8080}`);
