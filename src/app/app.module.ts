bc314e9da6a4d37f615a8635e8f85cc5b9e6dc05
HommyDeJesus <HommyDeJesus@DESKTOP-A7S1VC8>
2020-06-28 12:03:52 -0400

    Search pipe implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,18 @@ import { AppComponent } from './app.component';
 import { PokemonListComponent } from './components/pokemon/pokemon-list/pokemon-list.component';
 import { PokemonDetailsComponent } from './components/pokemon/pokemon-details/pokemon-details.component';
 import { SearchPipe } from './shared/pipes/search.pipe';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
+    SearchPipe,
     AppComponent,
     PokemonListComponent,
-    PokemonDetailsComponent,
-    SearchPipe
+    PokemonDetailsComponent, 
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     HttpClientModule,
     AppRoutingModule
